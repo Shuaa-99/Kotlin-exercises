eda3f572b4492059959e00bc9135a8c5c5e3ed2d
Shua'a <51505005+Shuaa-99@users.noreply.github.com>
2021-11-23 22:07:23 +0300

    Add files via upload
    Create a dictionary that uses numbers 1 - 7 for keys and days of the week as values
    Print each day of the week using a for loop

---
+++ Days of the Week.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val dayOfWeek = mapOf(1 to "Sunday", 2 to "Monday", 3 to "Tuesday", 4 to "Wednesday" , 5 to "Thursday" ,
+        6 to "Friday", 7 to "Saturday")
+    for(i in 1..7){
+        println(dayOfWeek[i])
+    }
+}
